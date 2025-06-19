89acf78c2f89a7c64a2829ab7778fa889c831ce5
Vijay Naik <vijaynaik@Vijays-MacBook-Pro.local>
2025-06-19 10:03:56 +0530

    initial files

---
+++ Basic_Addition.py
@@ -0,0 +1,14 @@
+Num1 = int(input("Enter the Num1 : "))
+Num2 = int(input("Enter the Num2 : "))
+
+addition = Num1 + Num2
+print("Addition", addition)
+
+sub = Num1 - Num2
+print("Substraction", sub)
+
+Mul = Num1 * Num2
+print("Multiplication", Mul)
+
+Div = Num1 / Num2
+print("Division", Div)
---
+++ concatation.py
@@ -0,0 +1,3 @@
+first_name = input("Enter First Name : ")
+last_name = input("Enter Last Name : ")
+print("Hello " + first_name + last_name + "! Welcome to the python program")
