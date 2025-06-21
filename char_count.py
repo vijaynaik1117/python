28b070040bbce092b54f9d3b85b2458601bcb792
Vijay Naik <vijaynaik@Vijays-MacBook-Pro.local>
2025-06-21 10:55:56 +0530

    assign2

---
+++ char_count.py
@@ -0,0 +1,6 @@
+word = "appleee"
+char_count = {}
+repeat_chat = []
+for i in word :
+    if char_count[i
+    char_count[i] = 
---
+++ sum_of _number.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+sum_of_number = 0
+for i in range(number):
+    sum_of_number += number
+
+print(sum_of_number)
+   
